28a04cd2a20494d26d326911fd176c98bc864d45
Ayesha Akbar <akbarayesha991@gmail.com>
2024-05-25 00:51:02 +0500

    first commit

---
+++ Question 10/index.ts
@@ -0,0 +1,18 @@
+//  Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+//  If you don’t have anything specific to write because your programs are too simple at this point, 
+//  just add your name and the current date at the top of each program file.
+//  Then write one sentence describing what the program does.
+
+
+// My Name is Ayesha
+// Dated 10-05-2024
+console.log("Economics is the knowledge of wealth.");
+
+
+// My Name is Ayesha
+// Dated 10-05-2024
+// Economics is everywhere, and understanding economics can help you make better decisions and lead a happier life.
+console.log("Economics is the branch of knowledge that relates to wealth.");
+
+
+
---
+++ Question 11/index.ts
@@ -0,0 +1,16 @@
+// Names: Store the names of a few of your friends in an array called names. 
+// Print each person’s name by accessing each element in the list, one at a time.
+
+
+let names: string[] = ['Ayesha','Bisma','Alisha','Ansha']
+
+console.log(names[0]);
+
+
+console.log(names[1]);
+
+
+console.log(names[2]);
+
+
+console.log(names[3]);
