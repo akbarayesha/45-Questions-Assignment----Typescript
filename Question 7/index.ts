82104fecc316e34a7f284a58278c049be56f687d
Ayesha Akbar <akbarayesha991@gmail.com>
2024-05-24 16:28:35 +0500

    first commit

---
+++ Question 7/index.ts
@@ -0,0 +1,14 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+//  Be sure to enclose your operations in print statements to see the results.
+
+console.log (3+5 ,'\tAddition');
+
+
+console.log (12-4 , '\tSubtraction');
+
+
+console.log (2*4 , '\tMultiplication'); 
+
+
+console.log (64/8 , '\tDivision');
+
