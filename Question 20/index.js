9e7262fbfdcf397660f533d74077e4cc93ce37de
Ayesha Akbar <akbarayesha991@gmail.com>
2024-05-25 05:48:56 +0500

    first commit

---
+++ Question 20/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// Think of something you could store in a array.
+// For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like.
+// Write a program that creates a list containing these items.
+let countries = ["Italy", "Argentina", "New Zealand", "Greece", "Switzerland"];
+console.log("List of Countries:");
+for (let list of countries) {
+    console.log(list);
+}
+;
