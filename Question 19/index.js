201a776466abe4d41cbac5078c4fb0978c42fb6e
Ayesha Akbar <akbarayesha991@gmail.com>
2024-05-25 05:20:38 +0500

    first commit

---
+++ Question 19/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+// print a message indicating the number of people you are inviting to dinner.
+let guest_invite = ["Ayesha", "Ansha"];
+let count_invite = guest_invite.length;
+console.log(`${count_invite} Guests are invited for the dinner!`);
---
+++ Question 19/index.ts
@@ -0,0 +1,11 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+// print a message indicating the number of people you are inviting to dinner.
+
+let guest_invite: string [] = ["Ayesha", "Ansha"]
+
+
+let count_invite: number = guest_invite.length
+
+
+console.log(`${count_invite} Guests are invited for the dinner!`);
+
