8921da18b52ea8273d6198b2bce5e299303ac572
Ayesha Akbar <akbarayesha991@gmail.com>
2024-05-26 03:49:32 +0500

    first commit

---
+++ Question 23/index.js
@@ -0,0 +1,41 @@
+"use strict";
+// Conditional Tests: Write a series of conditional tests.
+//  Print a statement describing each test and your prediction for the results of each test. 
+// Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+// Conditional Tests
+let car = "subaru";
+// Test 1
+console.log("is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+// Test 2
+console.log("is car != 'Honda City'? I predict True");
+console.log(car != 'Honda City');
+// Test 3
+console.log("is car == 'subaru'? I predict False");
+console.log(car == 'subaru');
+// Test 4
+console.log("is car == 'SUBARU'? I predict False");
+console.log(car == 'SUBARU');
+// Test 5
+console.log("is car.length == 5? I predict True");
+console.log(car.length == 5);
+// Test 6
+console.log("is car.length != 20? I predict True");
+console.log(car.length != 20);
+// Test 7
+console.log("is 20 > 40? I predict false");
+console.log(20 > 40);
+// Test 8
+console.log("is 7 <= 5? I predict false");
+console.log(7 <= 5);
+// Test 9
+console.log("is 74 > 79? I predict false");
+console.log(74 > 79);
+// Test 10
+console.log("is car == 'subaru' && car.length = 6? I predict true");
+console.log(car == 'subaru' && car.length == 6);
