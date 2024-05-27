34e16d7dc8d054e47f612a2fb69221fe42fcf012
Ayesha Akbar <akbarayesha991@gmail.com>
2024-05-28 03:15:07 +0500

    first commit

---
+++ Question 25/index.ts
@@ -0,0 +1,21 @@
+// Alien Colors #1: Imagine an alien was just shot down in a game.
+
+
+// Create a variable called Alien Color.
+
+let alien_color: string = "White";
+
+// Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+
+if(alien_color === 'White'){
+    console.log("Player just earned 5 points!")
+}
+
+// Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+
+// alien_color = "yellow";
+
+// if(alien_color === 'green'){
+//     console.log("Player just earned 5 points!")
+// }
+
