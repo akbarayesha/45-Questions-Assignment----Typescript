432b1e4e7ffc5b5fb756545bbb91cd26cc90ad6e
Ayesha Akbar <akbarayesha991@gmail.com>
2024-05-28 23:39:16 +0500

    first commit

---
+++ Question 37/index.ts
@@ -0,0 +1,15 @@
+// Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript.
+// Make a large shirt and a medium shirt with the default message, 
+// and a shirt of any size with a different message.
+
+
+function make_shirt(size: string = "Large", text: string = "Stay Positive!") {
+    console.log(`${size}, T-shirt with message: ${text}`);
+}
+
+make_shirt();
+
+make_shirt('Medium', 'Keep Calm and Carry On!');
+
+make_shirt('Small', 'Believe in Yourself!');
+
