36d2a3a7a3df4f34e832f629e0d95f54cc7fd2cf
Ayesha Akbar <akbarayesha991@gmail.com>
2024-05-29 00:47:57 +0500

    first commit

---
+++ Question 45/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Cars:Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+function carInfo(manufacturer, modelName, ...moreOptions) {
+    const carInfo = Object.assign({ manufacturer,
+        modelName }, Object.assign({}, ...moreOptions));
+    return carInfo;
+}
+let autoCar = carInfo('Toyota', 'Fortuner', { Color: 'Black' }, { Features: ['Four-Wheel Drive (4WD) Capability', 'Spacious Interior', 'Infotainment System', 'Multi-Terrain Select', 'Smart Key System with Push Button Start'] });
+console.log(autoCar);
