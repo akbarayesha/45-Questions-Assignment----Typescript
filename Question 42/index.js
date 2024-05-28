6252573c26568063bd1db54e79e27e53c95eaeee
Ayesha Akbar <akbarayesha991@gmail.com>
2024-05-29 00:22:25 +0500

    first commit

---
+++ Question 42/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// Great Magicians: Start with a copy of your program from Exercise 39. 
+// Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name.
+// Call show_magicians() to see that the list has actually been modified.
+let magicians = ['Penn & Teller', 'Dynamo', 'Shin Lim', 'Lance Burton', 'Juan Tamariz'];
+function show_magicians(magicians) {
+    magicians.forEach(element => {
+        console.log("The Great", element);
+    });
+}
+show_magicians(magicians);
---
+++ Question 42/index.ts
@@ -0,0 +1,15 @@
+// Great Magicians: Start with a copy of your program from Exercise 39. 
+// Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name.
+// Call show_magicians() to see that the list has actually been modified.
+
+let magicians: string [] = ['Penn & Teller','Dynamo','Shin Lim','Lance Burton','Juan Tamariz'];
+
+function show_magicians(magicians:string []) {
+    magicians.forEach(element => {
+        console.log("The Great",element);
+    });
+}
+
+show_magicians(magicians);
+
+
